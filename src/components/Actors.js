e447faa7d82aa4dd40b65d3e8fe2c99d2758c3d3
nachoespino53 <nachoespino53@gmail.com>
2018-12-02 18:42:38 -0500

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,16 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+  
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map(actor => <div className = 'actor'>
+      <h4>{actor.name}</h4>
+        <ul>
+          {actor.movies.map(movie => <li>{movie}</li>)}
+        </ul>
+      </div>)}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,16 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie => 
+          <div>
+              <h3>{movie.title}</h3>
+              <p>Movie Time: {movie.time}</p>
+              <ul>
+                {movie.genres.map(genre => <li>{genre}</li>)}
+              </ul>
+          </div>
+        )}
     </div>
   );
 };
